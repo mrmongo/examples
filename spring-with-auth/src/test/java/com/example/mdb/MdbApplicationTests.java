259e234374c2d291888b4cdece75343e3e30a447
Dorin Vinearschi <dvinearschi@crosscountry-home.com>
2018-03-19 14:43:43 -0400

    Initial code

---
+++ spring-with-auth/src/test/java/com/example/mdb/MdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MdbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
